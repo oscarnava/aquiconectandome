568c2cc0a09f8c8c18d7e4c6e8ae8ba5eb6f0726
Oscar Nava Trujillo <oscarnava@gmail.com>
2020-04-08 20:07:08 -0500

    Add custom display of cards

--- src/components/indexCard.js
+++ src/components/indexCard.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function IndexCard({ image }) {
-  return (
-    <div className="index-card">
-      <img src={`img/${image}`} alt="Card" />
-    </div>
-  );
+const STATUS_DONE = 'DONE';
+const STATUS_PENDING = 'PENDING';
+const STATUS_UNAVAILABLE = 'UNAVAILABLE';
+
+class IndexCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { status: STATUS_PENDING };
+  }
+
+  get content() {
+    const { status } = this.state;
+    const { image, text } = this.props;
+    switch (status) {
+      case STATUS_PENDING:
+        return (
+          <>
+            <img src={`img/${image}`} alt="Card" />
+            <p>{text}</p>
+            <textarea placeholder="Captura aquí tus pensamientos" rows="2" />
+          </>
+        );
+      case STATUS_DONE:
+        return <img src={`img/${image}`} alt="Card" />;
+
+      default:
+        return '';
+    }
+  }
+
+  render() {
+    return (
+      <div className="index-card">
+        { this.content }
+      </div>
+    );
+  }
 }
+
 
 IndexCard.propTypes = {
   image: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default IndexCard;
