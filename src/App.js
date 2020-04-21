2f467625e3dbbaf6ec0a04f504501cfb8738a52e
Oscar Nava Trujillo <oscarnava@gmail.com>
2020-04-20 19:50:10 -0500

    Add CubeMenu to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import request from './services/mockAPI';
 import IndexContainer from './containers/indexContainer';
+import CubeMenu from './components/cubeMenu';
+import './styles/app.sass';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { theme: 'amate' };
     this.indexContainer = React.createRef();
   }
 
   componentDidMount() {
     this.fetchIndexContents();
+  }
+
+  onMenuSelect = (option) => {
+    console.log({ option });
+    this.setState({ theme: option.toLowerCase() });
   }
 
   async fetchIndexContents() {
@@ -23,8 +31,15 @@ export default class App extends React.Component {
   }
 
   render() {
+    const { theme } = this.state;
     return (
-      <div className="app amate">
+      <div className={`app ${theme}`}>
+        <CubeMenu
+          options={['Amate', 'Wood', 'Day', 'Night']}
+          width="200px"
+          height="1.8rem"
+          onSelect={this.onMenuSelect}
+        />
         <IndexContainer ref={this.indexContainer} />
       </div>
     );
